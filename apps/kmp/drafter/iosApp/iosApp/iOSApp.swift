e1c27a21023bd1117883f237ddcda7fb5fd7d978
mr-saoirse <amartey@gmail.com>
2024-11-27 14:20:28 -0500

    code files

--- apps/kmp/drafter/iosApp/iosApp/iOSApp.swift
+++ apps/kmp/drafter/iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,27 @@
 import SwiftUI
 
+import FirebaseCore
+//import GoogleSignInSwift
+//import GoogleSignIn
+
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        FirebaseApp.configure()
+
+
+        return true
+    }
 }
---
+++ apps/kmp/drafter/shared/src/iosMain/kotlin/life/drafter/drafterapp/MainViewController.kt
@@ -0,0 +1,5 @@
+package life.drafter.drafterapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
