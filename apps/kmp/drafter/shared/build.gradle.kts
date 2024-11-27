d8576dec417c243d238fc66312483304abfbb234
mr-saoirse <amartey@gmail.com>
2024-11-27 13:52:27 -0500

    init firebase proj setup

--- apps/kmp/drafter/shared/build.gradle.kts
+++ apps/kmp/drafter/shared/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinCocoapods)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
@@ -30,11 +33,29 @@ kotlin {
             baseName = "shared"
             isStatic = true
         }
+
+        pod("GoogleSignIn") { linkOnly = true}
+        pod("FirebaseCore") { linkOnly = true }
     }
     
     sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+            //added
+            implementation(libs.play.services.auth)
+        }
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            //
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.firebase.auth)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
