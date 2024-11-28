a02f99e0152154cb760ad06f1d354305835c1170
mr-saoirse <amartey@gmail.com>
2024-11-28 08:36:16 -0500

    init proj

---
+++ apps/kmp/DrafterApp/composeApp/src/desktopMain/kotlin/life/drafter/drafterapp/main.kt
@@ -0,0 +1,13 @@
+package life.drafter.drafterapp
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "DrafterApp",
+    ) {
+        App()
+    }
+}
