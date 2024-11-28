2dc3019dc5d7ed6706a32889fc51b0aa255111cd
mr-saoirse <amartey@gmail.com>
2024-11-28 09:32:31 -0500

    App init

---
+++ apps/kmp/drafter/composeApp/src/desktopMain/kotlin/drafter/life/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package drafter.life
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
