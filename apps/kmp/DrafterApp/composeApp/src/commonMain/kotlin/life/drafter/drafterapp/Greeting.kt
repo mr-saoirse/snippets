d5e144b94a01a9263774b0a39896a65ebc76b445
mr-saoirse <amartey@gmail.com>
2024-11-28 08:46:46 -0500

    rem

--- apps/kmp/DrafterApp/composeApp/src/commonMain/kotlin/life/drafter/drafterapp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package life.drafter.drafterapp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- apps/kmp/DrafterApp/composeApp/src/commonMain/kotlin/life/drafter/drafterapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package life.drafter.drafterapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
